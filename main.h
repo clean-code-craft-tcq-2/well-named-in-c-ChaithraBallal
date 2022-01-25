2cf2e689dd904d283d720f293e489fa0c110835e
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-01-25 20:39:58 +0530

    Update main.h

--- main.h
+++ main.h
@@ -4,11 +4,6 @@
 #include<stdio.h>
 #include<assert.h>
 
-extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
-extern void testPairToNumber(enum MajorColor major, enum MinorColor minor,int expectedPairNumber);
-
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-
 /*type definitions */
 typedef struct 
 {
@@ -17,8 +12,9 @@ typedef struct
 } ColorPair;
 
 /* Function declarations */
+extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
 extern ColorPair GetColorFromPairNumber(int pairNumber);
-extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
-extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
 extern int GetPairNumberFromColor(const ColorPair* colorPair);
+extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+extern void testPairToNumber(enum MajorColor major, enum MinorColor minor,int expectedPairNumber);
 #endif /* MAIN_H */
