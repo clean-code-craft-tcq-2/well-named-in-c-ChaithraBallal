88399a3a6f0e45bbd749ad4c4d3e9af643725e9d
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-01-25 17:13:15 +0530

    Update ColorPairtoNum.h

--- ColorPairtoNum.h
+++ ColorPairtoNum.h
@@ -1,4 +1,8 @@
 #include <stdio.h>
 #include <assert.h>
 
+/*
+* Function Name: testPairtoNumber
+* Declaration: void testPairToNumber(enum MajorColor major, enum MinorColor minor,int expectedPairNumber);
+*/
 void testPairToNumber(enum MajorColor major, enum MinorColor minor,int expectedPairNumber);
