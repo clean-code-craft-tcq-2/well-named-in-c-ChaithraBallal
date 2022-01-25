b4483cf8f482185ecad477cd9609f29a60a8d224
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-01-25 18:47:28 +0530

    Update main.h

--- main.h
+++ main.h
@@ -5,3 +5,5 @@
 
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major, enum MinorColor minor,int expectedPairNumber);
+
+const int MAX_COLORPAIR_NAME_CHARS = 16;
