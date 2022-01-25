63a7bcefc7a02f059a294290b2e7dc7bfbdd0a5c
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-01-25 21:07:10 +0530

    Update main.h

--- main.h
+++ main.h
@@ -6,6 +6,9 @@
 
 /* Type definitions */
 
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
 typedef struct
 {
     enum MajorColor majorColor;
