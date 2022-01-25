f0b4faadfe1ff92eeb16c139b7e85bf92469dbaa
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-01-25 19:02:58 +0530

    Update ColorPair_NumberConversions.h

--- ColorPair_NumberConversions.h
+++ ColorPair_NumberConversions.h
@@ -1,9 +1,6 @@
 #include<stdio.h>
 #include<assert.h>
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
 typedef struct 
 {
     enum MajorColor majorColor;
