97b410d462e2465e5fb822c1bf245ef0723b62cf
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-01-25 21:44:17 +0530

    Update main.h

--- main.h
+++ main.h
@@ -20,6 +20,6 @@ extern ColorPair GetColorFromPairNumber(int pairNumber);
 extern int GetPairNumberFromColor(const ColorPair* colorPair);
 extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 extern void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
-extern void printReferenceManual(void);
+extern void PrintReferenceManual(void);
 
 #endif /* MAIN_H */
