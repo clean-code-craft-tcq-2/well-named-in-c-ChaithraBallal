d9f4721ee3d1f98e8585e092da80aa8209d0b072
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-01-25 18:41:15 +0530

    Delete NumtoColorPair.h

--- NumtoColorPair.h
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<assert.h>
-
-/*
-* Function Name: testNumberToPair
-* Purpose: verifies the color pair for the given number
-*/
-void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
