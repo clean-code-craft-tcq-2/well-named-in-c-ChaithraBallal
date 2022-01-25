fd6f54e8eb42da16a71fc0fde2b05aef453ff3a1
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-01-25 19:23:58 +0530

    Update ColorPair_NumberConversions.h

--- ColorPair_NumberConversions.h
+++ ColorPair_NumberConversions.h
@@ -1,17 +1,14 @@
 #include<stdio.h>
 #include<assert.h>
 
-typedef struct 
-{
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
-
 const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
 int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
 /* Function declarations */
 extern ColorPair GetColorFromPairNumber(int pairNumber);
