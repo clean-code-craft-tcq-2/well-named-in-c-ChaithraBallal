6e84ff166673e03fa6e145a7a031ea79ba5cb765
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-01-25 21:01:13 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,10 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
+/*
+ **********************************************************************************************************************
+ * Includes
+ **********************************************************************************************************************
+ */
+#include <stdio.h>
+#include <assert.h>
 
-#include<stdio.h>
-#include<assert.h>
-
-/* type definitions */
+/*
+ **********************************************************************************************************************
+ * Type definitions
+ **********************************************************************************************************************
+ */
 
 typedef struct
 {
@@ -12,10 +20,17 @@ typedef struct
     enum MinorColor minorColor;
 } ColorPair;
 
-/* Function declarations */
+/*
+ **********************************************************************************************************************
+ * Externals
+ **********************************************************************************************************************
+ */
+
 extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
 extern ColorPair GetColorFromPairNumber(int pairNumber);
 extern int GetPairNumberFromColor(const ColorPair* colorPair);
 extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
-extern void testPairToNumber(enum MajorColor major, enum MinorColor minor,int expectedPairNumber);
-#endif /* MAIN_H */
+extern void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+extern void printReferenceManual(void);
+
+#endif /* MAIN_H *
