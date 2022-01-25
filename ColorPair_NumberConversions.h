72454a85e534d3b251d0fa3baf845b249a8b3668
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-01-25 20:26:17 +0530

    Update ColorPair_NumberConversions.h

--- ColorPair_NumberConversions.h
+++ ColorPair_NumberConversions.h
@@ -1,6 +1,9 @@
-#include<stdio.h>
-#include<assert.h>
+ifndef COLORPAIR_NUMBERCONVERSIONS_H
+#define COLORPAIR_NUMBERCONVERSIONS_H
+
+/*type definitions*/
 
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
+#endif /*COLORPAIR_NUMBERCONVERSIONS_H*/
